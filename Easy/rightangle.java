6a0e75899fe50d9bdeb81bb63759da6ea9952ca8
saritha1987 <129212011+saritha1987@users.noreply.github.com>
2024-10-14 14:26:08 +0530

    Add files via upload

---
+++ Easy/rightangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class rightangle{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number:");
+        int n=sc.nextInt();
+        int i,j,k;
+        for(i=1;i<=n;i++){
+            for(k=1;k<=n-i;k++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
